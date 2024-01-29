321b16d34cd09f13f95ab6c46be4c4b88911c834
krzysiek1421 <68143000+krzysiek1421@users.noreply.github.com>
2024-01-29 15:02:01 +0100

    Adding sample index. Closes #4

--- src/main/java/pcs/springframework/pointsshow/controllers/TableViewController.java
+++ src/main/java/pcs/springframework/pointsshow/controllers/TableViewController.java
@@ -5,6 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import pcs.springframework.pointsshow.model.TableInfo;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Controller
 public class TableViewController {
     private final TableInfo tableInfo;
@@ -17,4 +22,28 @@ public class TableViewController {
         model.addAttribute("tableInfo", tableInfo);
         return "index";
     }
+
+    @GetMapping("/get-sample")
+    public String getSample(Model model) {
+        initializeSampleTableInfo();
+        model.addAttribute("tableInfo", tableInfo);
+        return "sampleIndex";
+    }
+
+
+    private void initializeSampleTableInfo() {
+        int numTeams = 5;
+        List<String> sampleCategories = Arrays.asList("Pop", "Metal", "Classical", "Jazz", "Rock");
+
+        tableInfo.setNumTeams(numTeams);
+        tableInfo.setCategories(sampleCategories);
+
+        List<List<Integer>> points = IntStream.range(0, sampleCategories.size())
+                .mapToObj(i -> IntStream.range(0, numTeams)
+                        .mapToObj(j -> 0)
+                        .collect(Collectors.toList()))
+                .collect(Collectors.toList());
+        tableInfo.setPoints(points);
+    }
+
 }
